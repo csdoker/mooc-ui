95a3355137ae4528ae2d96d8d3451083bf354e64
wanghuayu <583440138@qq.com>
2019-11-08 17:37:56 +0800

    badge组件开发

--- src/base/badge/badge.vue
+++ src/base/badge/badge.vue
@@ -1,10 +1,92 @@
 <template>
   <div class="mooc-badge">
-    badge
+    <slot></slot>
+    <span
+      v-show="showContent"
+      class="mooc-badge-content"
+      :class="[
+        `mooc-badge-content-${type}`,
+        {
+          'is-absolute': $slots.default,
+          'is-dot': isDot
+        }
+      ]"
+      v-text="content"
+    ></span>
   </div>
 </template>
 <script>
+import { baseType } from 'assets/js/mooc.config.js'
 export default {
-  name: 'MoocBadge'
+  name: 'MoocBadge',
+  props: {
+    value: [String, Number],
+    max: Number,
+    isDot: {
+      type: Boolean,
+      default: false
+    },
+    hidden: {
+      type: Boolean,
+      default: false
+    },
+    type: {
+      type: String,
+      default: 'danger',
+      validator (val) {
+        return baseType.includes(val)
+      }
+    }
+  },
+  computed: {
+    content () {
+      if (this.isDot) {
+        return ''
+      }
+      let value = +this.value
+      if (Number.isNaN(value)) {
+        return this.value
+      }
+      return value > this.max ? `${this.max}+` : value
+    },
+    showContent () {
+      return !this.hidden && (this.content || +this.content === 0 || this.isDot)
+    }
+  }
 }
 </script>
+<style lang="stylus" scoped>
+  @import '~assets/theme/variables.styl';
+  @import '~assets/theme/src/badge-variables.styl';
+  @import '~assets/theme/mixin/badge-mixin.styl';
+  .mooc-badge
+    position: relative
+    display: inline-block;
+    vertical-align: middle;
+    .mooc-badge-content
+      display: inline-block;
+      padding: $badge-veritcal-padding $badge-horizontal-padding;
+      height: $badge-size;
+      line-height: $badge-size;
+      border-radius: $badge-border-radius;
+      border: $badge-border-width $badge-border-style $badge-border-color;
+      background-color: $base-danger;
+      font-size: $badge-font-size;
+      color: $badge-color;
+      badge-status($base-primary, $base-success, $base-warning, $base-danger, $base-info);
+      &.is-absolute
+        position: absolute;
+        top: 0;
+        right: unit($badge-size / 2 + 1, 'px');
+        transform: translate(100%, -50%);
+        &.is-dot
+          right: $badge-dot-absolute-right;
+      &.is-dot
+        position: absolute;
+        top: 0;
+        right: 0;
+        padding: 0;
+        width: $badge-dot-size;
+        height: $badge-dot-size;
+        border-radius: $base-border-radius-circle;
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="home">
-    <mooc-badge></mooc-badge>
+    <mooc-badge type="primary" :value="10">
+      <div class="mooc-button">消息</div>
+    </mooc-badge>
+    <mooc-badge type="success" :value="100" :max="99">
+      <div class="mooc-button">消息</div>
+    </mooc-badge>
+    <mooc-badge :value="100" :is-dot="true">
+      <div class="mooc-button">消息</div>
+    </mooc-badge>
+    <mooc-badge value="hot">
+      <div class="mooc-button">消息</div>
+    </mooc-badge>
+    <mooc-badge value="hot" :hidden="true">
+      <div class="mooc-button">消息</div>
+    </mooc-badge>
   </div>
 </template>
 
@@ -18,5 +32,17 @@ export default {
   .home {
     margin: 100px auto;
     width: 800px;
+    .mooc-button {
+      width: 60px;
+      height: 36px;
+      text-align: center;
+      line-height: 36px;
+      border: 1px solid #58a;
+      color: #58a;
+      border-radius: 4px;
+    }
+    .mooc-badge {
+      margin-right: 30px;
+    }
   }
 </style>
