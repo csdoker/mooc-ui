61ae641a797a8384cc824f1f31698c68d4991e0b
wanghuayu <583440138@qq.com>
2019-11-12 21:40:57 +0800

    timeline组件开发

--- src/base/timeline/timeline.vue
+++ src/base/timeline/timeline.vue
@@ -1,11 +1,31 @@
-<template>
-  <div class="mooc-timeline">
-    <slot></slot>
-  </div>
-</template>
-
 <script>
 export default {
-  name: 'MoocTimeline'
+  name: 'MoocTimeline',
+  props: {
+    reverse: {
+      type: Boolean,
+      default: false
+    }
+  },
+  render () {
+    let reverse = this.reverse
+    let slots = this.$slots.default || []
+    let classs = {
+      'mooc-timeline': true
+    }
+    if (reverse) {
+      slots = slots.reverse()
+    }
+
+    return (
+      <ul class={ classs }>
+        { slots }
+      </ul>
+    )
+  }
 }
 </script>
+<style lang="stylus" scoped>
+  .mooc-timeline
+    list-style: none;
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,15 @@
 <template>
   <div class="home">
+    <mooc-timeline reverse>
+      <mooc-timeline-item timestamp="2019-11-12 17:53:56">123</mooc-timeline-item>
+      <mooc-timeline-item hide-timestamp placement="top" timestamp="2019-11-11 17:53:56">456</mooc-timeline-item>
+    </mooc-timeline>
     <mooc-timeline>
-      <mooc-timeline-item></mooc-timeline-item>
+      <mooc-timeline-item size="small" type="primary" timestamp="2019-11-12 17:53:56">123</mooc-timeline-item>
+      <mooc-timeline-item size="normal" type="success" timestamp="2019-11-11 17:53:56">456</mooc-timeline-item>
+      <mooc-timeline-item size="medium" type="warning" timestamp="2019-11-12 17:53:56">789</mooc-timeline-item>
+      <mooc-timeline-item size="large" type="danger" timestamp="2019-11-11 17:53:56">123</mooc-timeline-item>
+      <mooc-timeline-item size="large" type="info" timestamp="2019-11-11 17:53:56">456</mooc-timeline-item>
     </mooc-timeline>
   </div>
 </template>
@@ -21,5 +29,10 @@ export default {
   .home {
     margin: 100px auto;
     width: 800px;
+    .mooc-timeline {
+      display: inline-block;
+      vertical-align: top;
+      margin-right: 150px;
+    }
   }
 </style>
